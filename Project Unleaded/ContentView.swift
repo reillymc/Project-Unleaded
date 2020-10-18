0ef84f070b526ffc4219d958c0b7ded60f73affb
ReillyM-C <41772129+ReillyM-C@users.noreply.github.com>
2020-10-18 19:04:54 +1000

    Initial Commit

---
+++ Project Unleaded/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project Unleaded
+//
+//  Created by Reilly Mackenzie-Cree on 18/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project Unleaded/Project_UnleadedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_UnleadedApp.swift
+//  Project Unleaded
+//
+//  Created by Reilly Mackenzie-Cree on 18/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Project_UnleadedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
