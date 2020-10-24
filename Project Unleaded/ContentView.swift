f12064ede0dd481cbcbbaa36d7a8a18c495cff1a
ReillyM-C <41772129+ReillyM-C@users.noreply.github.com>
2020-10-24 15:19:04 +1000

    Implemented basic fuel list home screen, web and dummy apis and json processing

--- Project Unleaded/ContentView.swift
+++ Project Unleaded/ContentView.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var updated: String = ""
+    @State var regionList: [Region] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView{
+                LazyVStack() {
+                    ForEach(regionList) { region in
+                        Section(header: Text(region.region)) {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    if (region.region == "Australia"){
+                                        ForEach(region.prices) { priceData in
+                                            VStack{
+                                                Image(systemName: "car.fill").font(.largeTitle).padding(5)
+                                                Text("\(priceData.type)")
+                                                Text("$\(priceData.price, specifier: "%.1f¢")")
+                                                HStack{
+                                                    Image(systemName: "location").font(.footnote)
+                                                    Text("\(priceData.suburb), \(priceData.postcode)")
+                                                    
+                                                }
+                                            }
+                                            
+                                        }.frame(width: 240, height: 350, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color( UIColor(red: 0.84, green: 0.15, blue: 0.24, alpha: 1.00))
+                                        ).cornerRadius(20).shadow(radius: 5 ).padding(.all, 10)
+                                    } else {
+                                        ForEach(region.prices) { priceData in
+                                            VStack{
+                                                Image(systemName: "car.fill").font(.largeTitle).padding(5)
+                                                Text("\(priceData.type)")
+                                                Text("\(priceData.price, specifier: "%.1f")¢")
+                                                HStack{
+                                                    Image(systemName: "location").font(.footnote)
+                                                    Text("\(priceData.suburb), \(priceData.postcode)")
+                                                    
+                                                }
+                                            }
+                                            
+                                        }.frame(width: 240, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color( UIColor(red: 0.84, green: 0.15, blue: 0.24, alpha: 1.00))
+                                        ).cornerRadius(20).shadow(radius: 5 ).padding(.all, 10)
+                                        
+                                    }
+                                }.padding(.leading, 10)
+                                
+                            }
+                        }
+                    }
+                }
+                Text("Last Updated: \(updated)")
+            }.navigationBarTitle(Text("Cheapest Fuel"))
+        }
+        .onAppear {
+            API().getData(dummy: true) { (priceList) in
+                let formatter = DateFormatter()
+                formatter.timeStyle = .short
+                self.updated = formatter.string(from: Date(timeIntervalSince1970: Double(priceList.updated))
+                )
+                self.regionList = priceList.regions
+            }
+        }
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Project Unleaded/FuelData.swift
@@ -0,0 +1,94 @@
+//
+//  FuelData.swift
+//  Project Unleaded
+//
+//  Created by Reilly Mackenzie-Cree on 18/10/20.
+//
+
+import SwiftUI
+
+struct FuelData: Codable {
+    var updated: Int
+    var regions: [Region]
+}
+
+struct Region: Codable {
+    var region: String
+    var prices: [Price]
+}
+
+extension Region: Identifiable {
+    var id: String { return region }
+}
+
+struct Price: Codable, Hashable {
+    var type: String
+    var price: Double
+    var name: String
+    var state: String
+    var postcode: String
+    var suburb: String
+    var lat: Double
+    var lng: Double
+}
+
+extension Price: Identifiable {
+    var id: String { return type }
+}
+
+class API {
+    func getData(dummy: Bool, completion: @escaping (FuelData) -> ()) {
+        if (!dummy){
+            
+            guard let url = URL(string: "https://projectzerothree.info/api.php?format=json") else { return }
+            URLSession.shared.dataTask(with: url) { (data, _, _) in
+                var priceList = try! JSONDecoder().decode(FuelData.self, from: data!)
+                let formattedList = self.formatPriceList(priceList: &priceList)
+                DispatchQueue.main.async {
+                    completion(formattedList)
+                }
+            }
+            .resume()
+            
+        } else {
+            
+            let path = Bundle.main.path(forResource: "dummyapi.json", ofType: nil)
+            let jsonData = try! Data(contentsOf: URL(fileURLWithPath: path!))
+            var priceList = try! JSONDecoder().decode(FuelData.self, from: jsonData)
+            let formattedList = self.formatPriceList(priceList: &priceList)
+            DispatchQueue.main.async {
+                completion(formattedList)
+            }
+            
+        }
+        
+    }
+    
+    func formatPriceList(priceList: inout FuelData) -> FuelData {
+        priceList.regions[0].region = "Australia"
+        priceList.regions.removeAll { (region) -> Bool in
+            region.id.contains("-")
+        }
+        priceList.regions.sort { (a, b) -> Bool in
+            let defaultOrder = ["QLD", "NSW", "VIC", "ACT", "WA", "SA"]
+            if let first = defaultOrder.firstIndex(of: a.region), let second = defaultOrder.firstIndex(of: b.region) {
+                return first < second
+            }
+            return false
+        }
+        for index in priceList.regions.indices {
+            priceList.regions[index].prices.sort { (a, b) -> Bool in
+                let defaultOrder = ["U91", "U98", "E10", "U95", "Diesel", "LPG"]
+                if let first = defaultOrder.firstIndex(of: a.type), let second = defaultOrder.firstIndex(of: b.type) {
+                    return first < second
+                }
+                return false
+            }
+        }
+        
+        return priceList
+    }
+    
+}
+
+
